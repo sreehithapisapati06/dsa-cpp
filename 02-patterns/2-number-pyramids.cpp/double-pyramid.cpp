328c54b110d6489c78d85339e23f948c13de2dea
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-06-02 19:36:56 +0530

    Create double-pyramid.cpp

---
+++ 02-patterns/2-number-pyramids.cpp/double-pyramid.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number of lines: ";
+    cin>>n;
+
+for(int i=0; i<n; i++){
+        for(int j=1; j<=i+1; j++){
+            cout<<j;
+        }
+        for(int j=0; j<2*(n-i-1); j++){
+            cout<<" ";
+        }
+        for(int j=i+1; j>=1; j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+}
