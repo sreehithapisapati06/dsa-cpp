8f936979380f3dedc3db4a327e1112707d52e5a4
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-06-03 17:48:15 +0530

    Update 01-reverse-number.cpp

--- 01-basic-math-and-logic/01-reverse-number.cpp
+++ 01-basic-math-and-logic/01-reverse-number.cpp
@@ -1,1 +1,14 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int num=0;
+    while(n!=0){
+        int lastdigit=n%10;
+        num=num*10+lastdigit;
+        n=n/10;
+    }
+    cout<<num;
+    return 0;
+}
