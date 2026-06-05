b8bdbbc59293f5339b6994d72d72b324905a2dc3
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-06-05 17:10:46 +0530

    Update 03-armstrong-number.cpp

--- 01-basic-math-and-logic/03-armstrong-number.cpp
+++ 01-basic-math-and-logic/03-armstrong-number.cpp
@@ -1,1 +1,29 @@
-
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    int original = n;
+    int temp = n;
+    int digits = 0;
+    while (temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+    int sum = 0;
+    temp = n;
+    while (temp != 0) {
+        int last_digit = temp % 10;
+        int power = 1;
+        for (int i = 0; i < digits; i++) {
+            power *= last_digit;
+        }
+        sum += power;
+        temp /= 10;
+    }
+    if (sum == original) {
+        cout << "Armstrong Number";
+    } else {
+        cout << "Not an Armstrong Number";
+    }    return 0;
+}
