afe18f9043ecd29c8c9ef2c89f694cf580d6cb18
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-06-05 15:12:57 +0530

    Update 02-palindrom-check.cpp

--- 01-basic-math-and-logic/02-palindrom-check.cpp
+++ 01-basic-math-and-logic/02-palindrom-check.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int original=n;
+    int num=0;
+    while(n!=0){
+        int last_digit=n%10;
+        num=num*10+last_digit;
+        n=n/10;
+    }
+    if(original==num){
+        cout<<"palindrome number";
+    }
+    else{
+        cout<<"not a palindrome number";
+    }
+    return 0;
+}
