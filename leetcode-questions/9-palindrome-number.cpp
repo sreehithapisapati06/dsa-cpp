d80295d3bb534c0738f3ed062de9c3876db02f41
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-08-01 21:35:11 +0530

    Update 9-palindrome-number.cpp

--- leetcode-questions/9-palindrome-number.cpp
+++ leetcode-questions/9-palindrome-number.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long rev=0;
+        int original=x;
+        while(x>0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+        }
+        if(original==rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
