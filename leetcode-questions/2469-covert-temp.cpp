299f498c6d9b3a629af7279c99e92cad0a730482
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-08-02 16:40:37 +0530

    Update 2469-covert-temp.cpp

--- leetcode-questions/2469-covert-temp.cpp
+++ leetcode-questions/2469-covert-temp.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin;
+        double fahrenheit;
+        if(0<=celsius && celsius<=1000){
+            kelvin=celsius+273.15;
+            fahrenheit=celsius*1.80+32.00;
+            return {kelvin, fahrenheit};
+        }
+        return{};
+    }
+};
