96d836940564ff78419ff494152dfe3d61a43f5a
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-08-04 21:03:36 +0530

    Update 2651-delay-and-arrival.cpp

--- leetcode-questions/2651-delay-and-arrival.cpp
+++ leetcode-questions/2651-delay-and-arrival.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int time=arrivalTime+delayedTime;
+        if(1<=arrivalTime && arrivalTime<24 && 1<=delayedTime && delayedTime<=24){
+            if(time<=23){
+                return time;
+            }
+        
+        }
+        return time%=24;
+    }
+        
+        
+};
