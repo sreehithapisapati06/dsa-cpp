59a7e22eff36b4a1730ce9812f039502c36a8eeb
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-08-04 21:18:14 +0530

    Update 1344-angle-between-hands-of-clock.cpp

--- leetcode-questions/1344-angle-between-hands-of-clock.cpp
+++ leetcode-questions/1344-angle-between-hands-of-clock.cpp
@@ -1,1 +1,15 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        float h=(hour%12)*30+minutes*0.5;
+        float m=minutes*6;
+        double angle=fabs(h-m);
+        if(angle>180){
+            angle=360-angle;
+        }
+        return angle;
+    }
+        
+        
 
+};
