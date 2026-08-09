36418b9010681929426859f9717b1b97f93d59a6
Sreehitha Pisapati <pisapatisreehitha@gmail.com>
2026-08-09 16:00:05 +0530

    Create 1550-three-consecutive-odds.cpp

---
+++ leetcode-questions/1550-three-consecutive-odds.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int count=0;
+        for(int i=0; i+2<arr.size(); i++){
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0){
+                return true;
+            }
+        }
+        return false;
+    }
+};
